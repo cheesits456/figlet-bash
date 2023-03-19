c7551a3852dfc7bfc929d85fba8e89858456a35e
Quin <quin@cheesits456.dev>
2023-03-19 15:36:39 -0700

    Extend comment dividers to 80 chars

--- figlet.sh
+++ figlet.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# ---------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Static variables
 declare -r FULL_WIDTH=0
 declare -r FITTING=1
 declare -r SMUSHING=2
 declare -r CONTROLLED_SMUSHING=3
 
-# ---------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Variables that will hold information about the fonts
 declare -A figFonts
 declare -A figDefaults=([font]="Standard" [fontPath]="./fonts")
 
-# ---------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Functions
 
 # This function takes in the oldLayout and newLayout data from the FIGfont
